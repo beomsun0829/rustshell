0b5b90b75cb210ba5abc8b2b85a3ac5b4cd0158f
Hong Beomsun <ghsqjatns@gmail.com>
2025-06-04 02:17:02 +0900

    cd

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
+use std::env;
 use std::io::{stdin, stdout, Write};
+use std::path::Path;
 use std::process::Command;
 
 fn main() {
@@ -13,7 +15,18 @@ fn main() {
         let command = parts.next().unwrap();
         let args = parts;
 
-        let mut child = Command::new(command).args(args).spawn().unwrap();
-        let _ = child.wait();
+        match command {
+            "cd" => {
+                let new_dir = args.peekable().peek().map_or("/", |x| *x);
+                let root = Path::new(new_dir);
+                if let Err(e) = env::set_current_dir(&root) {
+                    eprintln!("{}", e);
+                }
+            }
+            _ => {
+                let mut child = Command::new(command).args(args).spawn().unwrap();
+                let _ = child.wait();
+            }
+        }
     }
 }

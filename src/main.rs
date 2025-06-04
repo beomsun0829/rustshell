2f677e57484a5f2117693af5cc85f74137e0bd2d
Hong Beomsun <ghsqjatns@gmail.com>
2025-06-04 13:52:30 +0900

    pipes

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::env;
 use std::io::{stdin, stdout, Write};
 use std::path::Path;
-use std::process::Command;
+use std::process::{Child, Command, Stdio};
 
 fn main() {
     loop {
@@ -11,21 +11,40 @@ fn main() {
         let mut input = String::new();
         stdin().read_line(&mut input).unwrap();
 
-        let mut parts = input.trim().split_whitespace();
-        let command = parts.next().unwrap();
-        let args = parts;
+        let mut commands = input.trim().split(" | ").peekable();
+        let mut previous_command: Option<std::process::Child> = None;
 
-        match command {
-            "cd" => {
-                let new_dir = args.peekable().peek().map_or("/", |x| *x);
-                let root = Path::new(new_dir);
-                if let Err(e) = env::set_current_dir(&root) {
-                    eprintln!("{}", e);
+        while let Some(command) = commands.next() {
+            let mut parts = command.trim().split_whitespace();
+            let command = parts.next().unwrap();
+            let args = parts;
+
+            match command {
+                "cd" => {
+                    let new_dir = args.peekable().peek().map_or("/", |x| *x);
+                    let root = Path::new(new_dir);
+                    if let Err(e) = env::set_current_dir(&root) {
+                        eprintln!("Error: {}", e);
+                    }
+
+                    previous_command = None;
                 }
-            }
-            _ => {
-                let mut child = Command::new(command).args(args).spawn().unwrap();
-                let _ = child.wait();
+                "exit" => return,
+                _ => {
+                    let stdin = previous_command.map_or(Stdio::inherit(), |output: Child| Stdio::from(output.stdout.unwrap()));
+                    let stdout = if commands.peek().is_some() { Stdio::piped() } else { Stdio::inherit() };
+
+                    let output = Command::new(command).args(args).stdin(stdin).stdout(stdout).spawn();
+                    match output {
+                        Ok(output) => {
+                            previous_command = Some(output);
+                        }
+                        Err(e) => {
+                            previous_command = None;
+                            eprintln!("Error: {}", e);
+                        }
+                    }
+                }
             }
         }
     }
